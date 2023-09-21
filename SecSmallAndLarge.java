a7e3c6c5f9b9f5c4fb7941651d04721d90107d7b
Mounika Seeram <105287249+Mounika1526@users.noreply.github.com>
2023-09-21 21:09:36 +0530

    Add files via upload

---
+++ SecSmallAndLarge.java
@@ -0,0 +1,59 @@
+ import java.util.Scanner;
+
+public class SecSmallAndLarge {
+    static private int secondSmallest(int[] arr, int n) {
+        if (n < 2) {
+            return -1;
+        }
+        int small = Integer.MAX_VALUE;
+        int second_small = Integer.MAX_VALUE;
+        int i;
+        for (i = 0; i < n; i++) {
+            if (arr[i] < small) {
+                second_small = small;
+                small = arr[i];
+            } else if (arr[i] < second_small && arr[i] != small) {
+                second_small = arr[i];
+            }
+        }
+        return second_small;
+    }
+
+    static private int secondLargest(int[] arr, int n) {
+        if (n < 2)
+            return -1;
+        int large = Integer.MIN_VALUE;
+        int second_large = Integer.MIN_VALUE;
+        int i;
+        for (i = 0; i < n; i++) {
+            if (arr[i] > large) {
+                second_large = large;
+                large = arr[i];
+            } else if (arr[i] > second_large && arr[i] != large) {
+                second_large = arr[i];
+            }
+        }
+        return second_large;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        int sS = secondSmallest(arr, n);
+        int sL = secondLargest(arr, n);
+        System.out.println("Second smallest is " + sS);
+        System.out.println("Second largest is " + sL);
+    }
+}
